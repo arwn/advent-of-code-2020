d156099ee0f108ece89825080f53638e4a7fd01c
Aren Windham <windham.aren@protonmail.com>
2020-12-01 19:20:21 -0800

    day-one

---
+++ one/two.py
@@ -0,0 +1,13 @@
+expenses = {}
+
+for line in open('expenses2', 'r'):
+    expenses[int(line)] = True
+
+for a in expenses:
+    for b in expenses:
+        want = 2020 - a - b
+        if want in expenses:
+            print(f"{a} * {b} * {want} = {a * b * want}")
+            exit(0)
+
+print("this should never happen")
