d4c47b355828ac404ec2e44a23ab111071eae156
Aren Windham <windham.aren@protonmail.com>
2020-12-05 00:41:44 -0800

    shorten s//

--- 05 Binary Boarding/both.pl
+++ 05 Binary Boarding/both.pl
@@ -5,10 +5,8 @@ use List::Util qw(max);
 use Array::Utils qw(array_diff);
 
 sub get_seatid {
-    s/R/1/g;
-    s/L/0/g;
-    s/F/0/g;
-    s/B/1/g;
+    s/R|B/1/g;
+    s/L|F/0/g;
     my $row    = sprintf( "%d", eval( "0b" . substr $_, 0, 7 ) );
     my $column = sprintf( "%d", eval( "0b" . substr $_, 7, 10 ) );
     my $seat_id = $row * 8 + $column;
