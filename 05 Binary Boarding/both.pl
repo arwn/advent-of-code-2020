e7bbd921828aeb8df3592be9ccc4646d85f636f8
Aren Windham <windham.aren@protonmail.com>
2020-12-05 00:36:16 -0800

    shortened get_seatid

--- 05 Binary Boarding/both.pl
+++ 05 Binary Boarding/both.pl
@@ -5,20 +5,12 @@ use List::Util qw(max);
 use Array::Utils qw(array_diff);
 
 sub get_seatid {
-    use integer;
-    my @data        = split //, $_;
-    my $row         = 0;
-    my $row_step    = 128;
-    my $column      = 0;
-    my $column_step = 8;
-    while (@data) {
-        for ( shift @data ) {
-            if (/B/) { $row_step /= 2; $row += $row_step }
-            if (/F/) { $row_step    /= 2; }
-            if (/R/) { $column_step /= 2; $column += $column_step }
-            if (/L/) { $column_step /= 2; }
-        }
-    }
+    s/R/1/g;
+    s/L/0/g;
+    s/F/0/g;
+    s/B/1/g;
+    my $row    = sprintf( "%d", eval( "0b" . substr $_, 0, 7 ) );
+    my $column = sprintf( "%d", eval( "0b" . substr $_, 7, 10 ) );
     my $seat_id = $row * 8 + $column;
     $seat_id;
 }
@@ -27,7 +19,7 @@ sub my_seatid {
     my @taken = sort { $a <=> $b } @_;
     my @all   = ( $taken[0] .. $taken[-1] );
     my @diff  = array_diff( @taken, @all );
-    return $diff[0];
+    $diff[0];
 }
 
 open my $fh, "<", "input" or die "cant open file";
